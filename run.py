a9d935de4932b8c11282cd7e43e3a720aa9f0fbd
Raul <raul@Rauls-MacBook-Pro.local>
2022-05-12 18:03:23 +0100

    Add function attack_roll
    This function takes as arg the number of D6 you want to throw,
    stores in a list and return the sum

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import random
+import math
 
 print("This is the first stage")
 
@@ -8,10 +9,16 @@ def chance_of_event():
     This is used to generated an event
     """
 
-def attack_roll():
+def attack_roll(number_of_dices):
     """
     This is used to generate the rolls for attacks. 
     """
+    results =[]
+    for dice in range(number_of_dices):
+        roll = random.randint(1,6)
+        results.append(roll)
+    attack_roll = sum(results)
+    return attack_roll
 
 def encounter():
     """
@@ -22,4 +29,4 @@ def fight():
     """
     Calculate the result of a fight
     """
-    
+print(attack_roll(3))
