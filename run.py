a074615b6213e7d2c1af26f5a06c3190c6cfcbfd
Raul <raul@Rauls-MacBook-Pro.local>
2022-05-12 17:33:38 +0100

    Add basic functions structure to run.py

--- run.py
+++ run.py
@@ -2,3 +2,24 @@
 import random
 
 print("This is the first stage")
+
+def chance_of_event():
+    """
+    This is used to generated an event
+    """
+
+def attack_roll():
+    """
+    This is used to generate the rolls for attacks. 
+    """
+
+def encounter():
+    """
+    This is used to represent the fight with a monster and advance to the next tower's level
+    """
+
+def fight():
+    """
+    Calculate the result of a fight
+    """
+    
