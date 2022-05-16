fe6f514fc5d50ba7301684746315a2ab9fe60336
Raul <raul@Rauls-MacBook-Pro.local>
2022-05-16 09:51:50 +0100

    Add basic monster class with random values
    Add basic monster class. This will class will be expanded
    after the basic logic is create and tested. Issue #4

--- run.py
+++ run.py
@@ -1,8 +1,23 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+from pydoc import ModuleScanner
 import random
 import math
 
 print("This is the first stage")
+
+class Monster:
+    """
+    This create an instance of monster
+    """
+    def __init__(self):
+
+        hp = random.randint(1,3)
+        attack = random.randint(2,3)
+        initiative  = random.randint(1,6)
+        self.hp = hp
+        self.attack = attack
+        self.initiative = initiative
+        
 
 def chance_of_event():
     """
@@ -25,8 +40,11 @@ def encounter():
     This is used to represent the fight with a monster and advance to the next tower's level
     """
 
+    print("A monster appear")
+
+
 def fight():
     """
     Calculate the result of a fight
     """
-print(attack_roll(3))
+
