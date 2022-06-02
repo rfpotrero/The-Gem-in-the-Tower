9e757b9418cdad058241f24dc86ed0b407a441b4
Raul <raul@Rauls-MacBook-Pro.local>
2022-06-02 20:58:23 +0100

    Add floor descriptions to floor functions
    Adding floor description to functions. This will allow the game
    to be further increased and will maintain run.py clear of text.

--- functions.py
+++ functions.py
@@ -2,8 +2,8 @@ import time
 
 def P_S(text):
     """
-    Print the one line of of text and wait a certain amount of time 
-    before printing the following one. This helps the player to 
+    Print the one line of of text and wait a certain amount of time
+    before printing the following one. This helps the player to
     follow the story.
 
     Credits to Deanna Carina https://github.com/DeannaCarina/StarTrekTimeLoop/blob/main/functions.py
@@ -13,37 +13,38 @@ def P_S(text):
     time.sleep(1)
 
 def intro():
-    """ 
+    """
     Displays the game game intro
     """
     P_S("The rest of your crew are missing or dead.")
     P_S("The few that survived the strom most likely did not survived the jungle")
-    P_S("Cutting through the thick wall of green is hard and takes time")
-    P_S("by the time you leave the behid the jungle your arms feel like")
-    P_S("if you have spend the entire day in battle.")
-    P_S("The fatigue fell like chains of iron over your limbs and so you")
-    P_S("decideto rest before the night close in")
+    P_S("The tower is the only human construction you can see since days")
+    P_S("and the gem on the top, as in the legends, shines like a light house")
+    P_S("guiding you towards the city.")
+    P_S(".....")
     P_S("")
 
 def first_floor():
     """
     This display the first floor description.
     """
-    P_S("You arrived at the gates of the city before mid day. The walls are")
-    P_S("empty. No sentinels, no people just the eerie silence around you.")
+    P_S("The gates of the city towering before you. The walls are empty")
+    P_S("No sentinels, no people just the eerie silence around you.")
     P_S("Placing your shoulder again the gate, it opened with a rasping")
     P_S("screech of rusty hinges.")
     P_S("The opened gate gave directly into a long, broad hall which ran away")
     P_S("and away until its vista grew indistinct in the distance.")
 
+
+
 def second_floor():
     """
     This display the second floor description.
     """
-    pass
+    P_S("Second floor descriotion!!")
 
 def thrid_floor():
     """
     This display the third floor description.
     """
-    pass
+    P_S("Third floor description!")
