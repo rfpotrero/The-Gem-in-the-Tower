52f9b57412ea888cb08d391ae62a56c84a69ddab
Raul <raul@Rauls-MacBook-Pro.local>
2022-06-05 12:29:15 +0100

    Add Thrid floor description to funcitons.py
    Adding thrird floor description to functions.py. Tracked in
    issue #23

--- functions.py
+++ functions.py
@@ -35,16 +35,29 @@ def first_floor():
     P_S("The opened gate gave directly into a long, broad hall which ran away")
     P_S("and away until its vista grew indistinct in the distance.")
 
-
-
 def second_floor():
     """
     This display the second floor description.
     """
-    P_S("Second floor descriotion!!")
+    P_S("Your eyes widenen after reaching the end of the stairs")
+    P_S("Before you appear what is seems a completed city the roof missing in the distance")
+    P_S("No chambers, no gates. These people built their entery city inside.")
+    P_S("Talls building surrounding you and the same eerie feeling floats around you")
+    P_S("Searching through some of the shops and houses you see similar scenes")
+    P_S("interrupted meals, works half finished but no bodies to be seen...")
+    
 
 def thrid_floor():
     """
     This display the third floor description.
     """
-    P_S("Third floor description!")
+    P_S("The climbs ends up in a patio that even if it has seen better day")
+    P_S("still you can tell it was constructed with the most expensive materials")
+    P_S("rich marmol and jade mosaics cover the walls.")
+    P_S("This probably is were the rulers and rich used to live")
+    P_S("Massive villas like can be seen in the distance and a what probably")
+    P_S("Is the access to top of the tower. A massive stairs of white stone")
+    P_S("reflecting the light that enter through huge windows")
+    P_S("you start to make your way to the stair.")
+    P_S("Passing in front of what looks like luxurious shops you wonder")
+    P_S("if there will be something of value left...")
